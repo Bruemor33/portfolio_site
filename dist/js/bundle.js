e7347edcfaf668059b984163c638125f1104cb3c
Bruemor <arymer1@gmail.com>
2016-05-26 11:13:05 -0400

    commiting dist for gh-pages to work

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
   $('.scroll-link').on('click', function(event){
     event.preventDefault();
     var sectionID = $(this).attr("data-id");
-    scrollToID('#' + sectionID, 750);
+    scrollToID('#' + sectionID, 900);
   });
 
   //scroll to top functionality
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
 //scroll function
 function scrollToID(id, speed){
-  var offSet = 50;
+  var offSet = 100;
   var targetOffset = $(id).offset().top - offSet;
   var mainNav = $('#main-nav');
   $('html,body').animate({scrollTop:targetOffset}, speed);
