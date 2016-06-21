dfe535a5443394ba2fccd7bc78221a6709880ec2
Bruemor <arymer1@gmail.com>
2016-06-21 11:05:13 -0400

    changed the offset value in the scroll script in order to fix a spacing issue I was having with the scrolling nave.

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
   $('.scroll-link').on('click', function(event){
     event.preventDefault();
     var sectionID = $(this).attr("data-id");
-    scrollToID('#' + sectionID, 900);
+    scrollToID('#' + sectionID, 700);
   });
 
   //scroll to top functionality
@@ -30,7 +30,7 @@ $(document).ready(function(){
 
 //scroll function
 function scrollToID(id, speed){
-  var offSet = 100;
+  var offSet = 50;
   var targetOffset = $(id).offset().top - offSet;
   var mainNav = $('#main-nav');
   $('html,body').animate({scrollTop:targetOffset}, speed);
